b5b9be1c21045da49ea5f0945af81c79ce4db0a1
Alejandro del Río <adelriosantiago@gmail.com>
2019-02-14 02:10:52 -0600

    Print doc version and content with fetch()

--- server.js
+++ server.js
@@ -1,32 +1,46 @@
 var http = require('http');
 var express = require('express');
-
-var ShareDB = require('sharedb');
-var WebSocket = require('ws');
-var WebSocketJSONStream = require('websocket-json-stream');
-
 var app = express();
 app.use(express.static('public'));
 var server = http.createServer(app);
 
-var backend = new ShareDB();
-var connection = backend.connect();
+//BoyDog module
+var boydog = function(server) {
+  var ShareDB = require('sharedb');
+  var WebSocket = require('ws');
+  var WebSocketJSONStream = require('websocket-json-stream');
+  
+  var scope = {};
 
-var doc = connection.get('examples', 'randomABC');
-doc.fetch(function(err) {
-  if (err) throw err;
-  if (doc.type === null) {
-    doc.create({ content: '' });
-    return;
-  }
-});
+  var backend = new ShareDB();
+  var connection = backend.connect();
 
-// Connect any incoming WebSocket connection to ShareDB
-var wss = new WebSocket.Server({server: server});
-wss.on('connection', function(ws, req) {
-  var stream = new WebSocketJSONStream(ws);
-  backend.listen(stream);
-});
+  var doc = connection.get('examples', 'randomABC');
+  doc.fetch(function(err) {
+    if (err) throw err;
+    if (doc.type === null) {
+      doc.create({ content: 'abc123' });
+      return;
+    }
+  });
+
+  // Connect any incoming WebSocket connection to ShareDB
+  var wss = new WebSocket.Server({ server });
+  wss.on('connection', function(ws, req) {
+    var stream = new WebSocketJSONStream(ws);
+    backend.listen(stream);
+  });
+  
+  return { doc };
+}
+
+var boy = boydog(server);
+console.log("boy", boy);
+
+setInterval(function(){
+  boy.doc.fetch();
+  console.log(`doc ${ boy.doc.version } - ${ boy.doc.data.content }`);
+}, 1000);
 
 server.listen(7873);
 console.log('Listening on http://localhost:7873');
