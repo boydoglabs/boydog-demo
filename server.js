60319830db0eb95190f5a761112dafb0cd1ba699
Alejandro del Río <adelriosantiago@gmail.com>
2019-02-25 00:31:36 -0600

    Add functionality for 3 elements Working with 3 elements

--- server.js
+++ server.js
@@ -6,42 +6,26 @@ var port = 7873;
 var boydog = require("boydog");
 var boy = boydog(server);
 
-var scope = {
-  word: "abc",
-  title: "qwe",
-  random: "iop",
-};
-
-boy.attach(scope);
-
 app.use(express.static("public"));
 
 app.get("/restart", function(req, res) {
   boy.restart();
-  
+
   return res.json({ ok: true });
 });
 
 app.get("/test", function(req, res) {
-  boy.doc.fetch(err => {
-    return res.json({ v: boy.doc.version, data: boy.doc.data });
-  });
+  return res.json({ test: "ok" });
 });
 
-app.get("/testChange", function(req, res) {
-  boy.doc.fetch(() => {
-    var op = [{ p: ["content"], t: "text0", o: [{ p: 0, i: "XYZ" }] }];
-    boy.doc.submitOp(op, error => {
-      if (error) {
-        console.error("err", error);
-      } else {
-        console.log("success");
-      }
-    });
+//Boydog init
+var scope = {
+  word: "abc",
+  title: "qwe",
+  random: "iop"
+};
 
-    return res.json({ change: "ok" });
-  });
-});
+boy.attach(scope);
 
 server.listen(port);
 console.log("Listening on http://localhost:" + port);
